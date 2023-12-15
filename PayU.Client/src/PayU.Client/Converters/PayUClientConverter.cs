2b3971d0ceade0c259cbe946b29b6e8c9c107847
Jaroslaw Abram <jarek.abram1@gmail.com>
2023-12-15 17:02:43 +0100

    Update to .net6

--- PayU.Client/src/PayU.Client/Converters/PayUClientConverter.cs
+++ PayU.Client/src/PayU.Client/Converters/PayUClientConverter.cs
@@ -1,29 +1,42 @@
-using System;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using PayU.Client.Models;
 
-namespace PayU.Client.Converters
+namespace PayU.Client.Converters;
+
+public static class PayUClientConverter
 {
-    public static class PayUClientConverter
+    private const string OldValueLin = "\";\n";
+    private const string OldValueWin = "\";\r\n";
+    private const string NewValue = "\",\n";
+
+    public static T DeserializeResponse<T>(string value)
+        where T : class
     {
-        private const string OldValue = "\";\n";
-        private const string NewValue = "\",\n";
-
-        public static T DeserializeResponse<T>(string value)
-            where T : class
+        if (typeof(T) == typeof(RetrivePayoutResponse))
         {
-            if (typeof(T) == typeof(RetrivePayoutResponse))
-            {
-                return JsonConvert.DeserializeObject<T>(FixBadJsonResponse(value));
-            }
-
-            return JsonConvert.DeserializeObject<T>(value);
+            return JsonConvert.DeserializeObject<T>(FixBadJsonResponse(value));
         }
 
+        return JsonConvert.DeserializeObject<T>(value);
+    }
 
-        private static string FixBadJsonResponse(string value)
+
+    private static string FixBadJsonResponse(string value)
+    {
+        var regex = new Regex(OldValueLin);
+
+        if (regex.IsMatch(value))
         {
-            return value.Replace(OldValue, NewValue);
+            return regex.Replace(value, NewValue);
         }
+
+        regex = new Regex(OldValueWin);
+        if (regex.IsMatch(value))
+        {
+            return regex.Replace(value, NewValue);
+        }
+
+        return value;
     }
 }
